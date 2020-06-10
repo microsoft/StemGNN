475c58767201730c021034fc97146d0deb6d03d5
t-decao <66416829+t-decao@users.noreply.github.com>
2020-06-10 21:25:41 +0800

    add main.py

--- main.py
+++ main.py
@@ -19,20 +19,22 @@ from models.tester import model_test
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--n_route', type=int, default=228)
+parser.add_argument('--n_route', type=int, default=140)
 parser.add_argument('--n_his', type=int, default=12)
 parser.add_argument('--n_pred', type=int, default=3)
 parser.add_argument('--batch_size', type=int, default=50)
 parser.add_argument('--epoch', type=int, default=50)
-parser.add_argument('--save', type=int, default=10)
+parser.add_argument('--save', type=int, default=1)
 parser.add_argument('--ks', type=int, default=3)
 parser.add_argument('--kt', type=int, default=3)
 parser.add_argument('--lr', type=float, default=1e-3)
 parser.add_argument('--opt', type=str, default='RMSProp')
 parser.add_argument('--graph', type=str, default='default')
 parser.add_argument('--inf_mode', type=str, default='merge')
-parser.add_argument('--dataset', type=str, default='./dataset/PeMS07.csv')
-parser.add_argument('--output_dir', type=str, default='./output/PeMS07')
+parser.add_argument('--dataset', type=str, default='./dataset/ECG_data.csv')
+parser.add_argument('--model_dir', type=str, default='./output/ECG_data')
+parser.add_argument('--train', type=str, default='True')
+parser.add_argument('--evaluate', type=str, default='True')
 
 args = parser.parse_args()
 print(f'Training configs: {args}')
@@ -41,28 +43,30 @@ n, n_his, n_pred = args.n_route, args.n_his, args.n_pred
 Ks, Kt = args.ks, args.kt
 # blocks: settings of channel size in Spe-Seq Cell
 blocks = [[1, 32, 64], [64, 32, 128]]
-output_dir = args.output_dir
+output_dir = args.model_dir
 
 # Data Preprocessing
 data_file = args.dataset
-n_train, n_val, n_test = 34, 5, 5
-PeMS = data_gen(data_file, (n_train, n_val, n_test), n, n_his + n_pred)
-print(f'>> Loading dataset with Mean: {PeMS.mean:.2f}, STD: {PeMS.std:.2f}')
 
-SEED = 0
+DATA = data_gen(data_file, n, n_his + n_pred)
+print(f'>> Loading dataset ')
+
+SEED = 1
 if __name__ == '__main__':
-    os.environ['PYTHONHASHSEED'] = str(SEED)
-    # os.environ['TF_CUDNN_DETERMINISTIC'] = str(SEED) # new flag in tf 2.0+
     tf.compat.v1.set_random_seed(SEED)
     random.seed(SEED)
     np.random.seed(SEED)
-
-    before_train = datetime.now().timestamp()
-    model_train(PeMS, blocks, args, tensorboard_summary_dir=pjoin(output_dir, 'tensorboard'),
-                model_dir=pjoin(output_dir, 'model'))
-    after_train = datetime.now().timestamp()
-    model_test(PeMS, PeMS.get_len('test'), n_his, n_pred, args.inf_mode, load_path=pjoin(output_dir, 'model'))
-    after_evaluation = datetime.now().timestamp()
+    if args.train == 'True':
+        before_train = datetime.now().timestamp()
+        model_train(DATA, blocks, args, tensorboard_summary_dir=pjoin(output_dir, 'tensorboard'),
+                    model_dir=pjoin(output_dir, 'model'))
+        after_train = datetime.now().timestamp()
+    if args.evaluate == 'True':
+        before_evaluation = datetime.now().timestamp()
+        model_test(DATA, DATA.get_len('test'), n_his, n_pred, args.inf_mode, load_path=pjoin(output_dir, 'model'))
+        after_evaluation = datetime.now().timestamp()
     print('Duration Overview:')
-    print(f'Training took {(after_train - before_train)/60} minutes')
-    print(f'Evaluation took {(after_evaluation - after_train)/60} minutes')
+    if args.train == 'True':
+        print(f'Training took {(after_train - before_train)/60} minutes')
+    if args.evaluate == 'True':
+        print(f'Evaluation took {(after_evaluation - before_evaluation)/60} minutes')
