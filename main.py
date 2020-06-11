7c8d203bc4baf54b573781aa34901b28a6360e22
t-decao <66416829+t-decao@users.noreply.github.com>
2020-06-11 10:34:36 +0800

    change train to bool

--- main.py
+++ main.py
@@ -33,8 +33,8 @@ parser.add_argument('--graph', type=str, default='default')
 parser.add_argument('--inf_mode', type=str, default='merge')
 parser.add_argument('--dataset', type=str, default='./dataset/ECG_data.csv')
 parser.add_argument('--output_dir', type=str, default='./output/ECG_data')
-parser.add_argument('--train', type=str, default='True')
-parser.add_argument('--evaluate', type=str, default='True')
+parser.add_argument('--train', type=bool, default=True)
+parser.add_argument('--evaluate', type=bool, default=False)
 
 args = parser.parse_args()
 print(f'Training configs: {args}')
@@ -56,17 +56,17 @@ if __name__ == '__main__':
     tf.compat.v1.set_random_seed(SEED)
     random.seed(SEED)
     np.random.seed(SEED)
-    if args.train == 'True':
+    if args.train :
         before_train = datetime.now().timestamp()
         model_train(DATA, blocks, args, tensorboard_summary_dir=pjoin(output_dir, 'tensorboard'),
                     model_dir=pjoin(output_dir, 'model'))
         after_train = datetime.now().timestamp()
-    if args.evaluate == 'True':
+    if args.evaluate :
         before_evaluation = datetime.now().timestamp()
         model_test(DATA, DATA.get_len('test'), n_his, n_pred, args.inf_mode, load_path=pjoin(output_dir, 'model'))
         after_evaluation = datetime.now().timestamp()
     print('Duration Overview:')
-    if args.train == 'True':
+    if args.train :
         print(f'Training took {(after_train - before_train)/60} minutes')
-    if args.evaluate == 'True':
+    if args.evaluate :
         print(f'Evaluation took {(after_evaluation - before_evaluation)/60} minutes')
