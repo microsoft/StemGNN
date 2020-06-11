8cfe0649a464070ce97f89ec27bb855a59a259ba
t-decao <66416829+t-decao@users.noreply.github.com>
2020-06-11 17:06:59 +0800

    Add files via upload

--- data_loader/data_utils.py
+++ data_loader/data_utils.py
@@ -3,6 +3,8 @@ from utils.math_utils import z_score
 
 import numpy as np
 import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+#import numpy as np
 
 
 class Dataset(object):
@@ -38,11 +40,14 @@ def seq_gen(len_seq, data_seq, offset, n_frame, n_route, day_slot, C_0=1):
     :return: np.ndarray, [len_seq, n_frame, n_route, C_0].
     '''
     n_slot = day_slot - n_frame + 1
-
-    if len_seq < 1 :
-        total = int(len(data_seq) / n_slot) - 3
+    if len_seq<1:
+        if len(data_seq) / n_slot < 50 :
+            total = int(len(data_seq) / n_slot) - 5
+        else:
+            total = int(len(data_seq) / n_slot) - 6
         len_seq = int(total * len_seq)
         offset = int(total * offset)
+
 
     tmp_seq = np.zeros((len_seq * n_slot, n_frame, n_route, C_0))
     for i in range(len_seq):
@@ -53,7 +58,7 @@ def seq_gen(len_seq, data_seq, offset, n_frame, n_route, day_slot, C_0=1):
     return tmp_seq
 
 
-def data_gen(file_path, n_route, n_frame=21, day_slot=288):
+def data_gen(file_path, n_route, scalar, n_frame=21, day_slot=288):
     '''
     Source file load and dataset generation.
     :param file_path: str, the file path of data source.
@@ -64,54 +69,40 @@ def data_gen(file_path, n_route, n_frame=21, day_slot=288):
     :param day_slot: int, the number of time slots per day, controlled by the time window (5 min as default).
     :return: dict, dataset that contains training, validation and test with stats.
     '''
-
+    n_train, n_val, n_test = 0.6, 0.2, 0.2
     # generate training, validation and test data
     try:
 
-        if n_route == 228:
-            data_seq = pd.read_csv(file_path, header=None)  # .values
-            # for column in list(data_seq.columns):
-            #     #print(column)
-            #     mean_val = data_seq[column].mean()
-            #     data_seq[column].replace(0, mean_val, inplace=True)
-            data_seq = data_seq.values
-            n_train, n_val, n_test = 34,5,5
-        elif n_route == 140:
-            from sklearn.preprocessing import MinMaxScaler
-            import numpy as np
+        data_seq = pd.read_csv(file_path)
+        data_seq.drop(['label'],axis=1,inplace=True)
+        data_seq.drop(['set'],axis=1,inplace=True)
+        #data_seq = pd.read_csv(file_path, header=None)  # .values
+        # for column in list(data_seq.columns):
+        #         #print(column)
+        #     mean_val = data_seq[column].mean()
+        #     data_seq[column].replace(0, mean_val, inplace=True)
+        data_seq = data_seq.values
 
-            data_seq = pd.read_csv(file_path)
-            data_seq.drop(['label'],axis=1,inplace=True)
-            data_seq.drop(['set'],axis=1,inplace=True)
-            data_seq=data_seq.values
-            #my_matrix = np.loadtxt(open("xxxx.csv"),delimiter=",",skiprows=0)
+        if scalar == 'min_max':
             my_matrix = np.array(data_seq)
-
-            scaler = MinMaxScaler( )
+            scaler = MinMaxScaler()
             scaler.fit(my_matrix)
             scaler.data_max_
-            data_seq=scaler.transform(my_matrix)
-            n_train, n_val, n_test = 15,1,1
-        else:
-            data_seq = pd.read_csv(file_path, header=None)  # .values
-            for column in list(data_seq.columns):
-                #print(column)
-                mean_val = data_seq[column].mean()
-                data_seq[column].replace(0, mean_val, inplace=True)
-            data_seq = data_seq.values
-            n_train, n_val, n_test = 0.6, 0.2, 0.2
+            data_seq = scaler.transform(my_matrix)
+
         
 #        data_seq=data_seq[~(data_seq==0).all(axis=1), :]
         print(data_seq.shape)
     except FileNotFoundError:
         print(f'ERROR: input file was not found in {file_path}.')
 
-    seq_train = seq_gen(n_train, data_seq, 0, n_frame, n_route, day_slot)
-    seq_val = seq_gen(n_val, data_seq, n_train, n_frame, n_route, day_slot)
-    seq_test = seq_gen(n_test, data_seq, n_train + n_val, n_frame, n_route, day_slot)
+    seq_test = seq_gen(n_test, data_seq, 0, n_frame, n_route, day_slot)
+    seq_val = seq_gen(n_val, data_seq, n_test, n_frame, n_route, day_slot)
+    seq_train = seq_gen(n_train, data_seq, n_test+n_val, n_frame, n_route, day_slot)
 
-    x_stats = {'mean': np.mean(seq_train), 'std': np.std(seq_train)}
-    if n_route == 140:
+    if scalar == 'z_score':
+        x_stats = {'mean': np.mean(seq_train), 'std': np.std(seq_train)}
+    elif scalar == 'min_max':
         x_stats = {'mean': 0, 'std': 1}
 
     # x_stats: dict, the stats for the train dataset, including the value of mean and standard deviation.
@@ -121,11 +112,7 @@ def data_gen(file_path, n_route, n_frame=21, day_slot=288):
     x_train = z_score(seq_train, x_stats['mean'], x_stats['std'])
     x_val = z_score(seq_val, x_stats['mean'], x_stats['std'])
     x_test = z_score(seq_test, x_stats['mean'], x_stats['std'])
-    # x_train = z_score(seq_train, x_stats['mean'], x_stats['std'])
-    # x_val = z_score(seq_val, x_stats['mean'], x_stats['std'])
-    # x_test = z_score(seq_test, x_stats['mean'], x_stats['std'])
-    if n_route == 137:
-        x_stats = {'mean': 0, 'std': 1}
+
 
     x_data = {'train': x_train, 'val': x_val, 'test': x_test}
     dataset = Dataset(x_data, x_stats)
