e03a6157f67ae0ceabe9cfd9c6aa64187994a2ff
Joey <cazhiyu84@gmail.com>
2023-03-02 23:22:00 -0800

    Modified the content of data pre-processing. Deleted the code that discarded the last 5 rows when reading .csv file, and read the entire file instead. If there are some special reasons, please uncomment the comment line.

--- ce290_data/data_cleaning.py
+++ ce290_data/data_cleaning.py
@@ -1,8 +1,35 @@
 import pandas as pd
 
-file = 'jan2020.csv'
-df = pd.read_csv(f'ce290_data/{file}', header=1).iloc[:-5,:]
-df = df[['Departure', 'Arrival', 'Date', 'Flight\nCount', 'Airport\nDeparture\nDelay', 'Airborne\nDelay','Gate\nArrival\nDelay']]
-df.columns = ['depature', 'arrival', 'date', 'number_of_flights', 'departure_airport_delay', 'airborne_delay','arrival_gate_delay']
-df.head(10)
+class data_processing:
+    """
+    Process the data from specified path.
+    """
+    def __init__(self, path):
+        self.path = path
+
+    def data_cleaning(self):
+        df = pd.read_csv(f'./{self.path}', header=1)
+        # df = pd.read_csv(f'./{self.path}', header=1).iloc[:-5, :]
+        df = df[['Departure',
+                 'Arrival',
+                 'Date',
+                 'Flight\r\nCount',
+                 'Airport\r\nDeparture\r\nDelay',
+                 'Airborne\r\nDelay',
+                 'Gate\r\nArrival\r\nDelay']]
+        df.columns = ['depature',
+                      'arrival',
+                      'date',
+                      'number_of_flights',
+                      'departure_airport_delay',
+                      'airborne_delay',
+                      'arrival_gate_delay']
+        pd.set_option('display.max_columns', None)
+        # print(df.head(10))
+        return df
+
+## Example
+# processor = data_processing('jan2020.csv')
+# df_1 = processor.data_cleaning()
+# print(df_1)
 
