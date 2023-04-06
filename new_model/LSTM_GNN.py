73476d2d2790849146e247c3174c32b3574c8f21
caoalbert <zyfxdtn@gmail.com>
2023-04-06 10:34:26 -0700

    packaged NN

--- new_model/LSTM_GNN.py
+++ new_model/LSTM_GNN.py
@@ -6,14 +6,16 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 
 class DataLoader:
-    def __init__(self, data_path, n_batch, n_timesteps, n_pred):
+    def __init__(self, data_path, start_day, n_training, n_timesteps, n_pred):
         self.x = pd.read_csv(data_path).to_numpy()
-        self.n_batch = n_batch
+        self.start_day = start_day
         self.n_timesteps = n_timesteps
+        self.n_training = n_training
         self.n_pred = n_pred
+        self.n_batch = n_training-n_timesteps-n_pred
 
     def train_test_split(self):
-        self.train = self.x[:,:self.n_batch+self.n_timesteps+self.n_pred]
+        self.train = self.x[:,self.start_day:self.start_day+self.n_training]
         self.scale = StandardScaler()
         self.train = self.scale.fit_transform(self.train.T).T
         self.x_train = np.zeros(shape=(self.n_batch, self.n_timesteps, 77))
@@ -21,41 +23,67 @@ class DataLoader:
         for i in range(self.n_batch):
             self.x_train[i,:,:] = self.train[:,i:i+self.n_timesteps].T
             self.y_train[i,:] = self.train[:,i+self.n_timesteps:i+self.n_timesteps+self.n_pred].flatten()
-        
-        self.test = self.x[:,self.n_batch+self.n_timesteps+self.n_pred:]
+
+        # +1 is added here to resolve the issue that the state of LSTM is 0 for the first element
+        self.test = self.x[:,self.start_day+self.n_pred+1:]
         self.test = self.scale.transform(self.test.T).T
         self.x_test = np.zeros(shape=(self.n_batch, self.n_timesteps, 77))
-        self.y_test = np.zeros(shape=(self.n_batch, 77*self.n_pred))
         for i in range(self.n_batch):
             self.x_test[i,:,:] = self.test[:,i:i+self.n_timesteps].T
-            self.y_test[i,:] = self.test[:,i:i+self.n_pred].flatten()
-
-        return tf.convert_to_tensor(self.x_train), tf.convert_to_tensor(self.y_train), tf.convert_to_tensor(self.x_test), tf.convert_to_tensor(self.y_test)
+        return tf.convert_to_tensor(self.x_train), tf.convert_to_tensor(self.y_train), tf.convert_to_tensor(self.x_test)
 
 class LSTMoutput:
-    def __init__(self, DataLoader, n_batch, n_timesteps, airport_index, series_index):
-        self.n_timesteps = n_timesteps
+    def __init__(self, DataLoader, airport_index):
+        self.actual = DataLoader.x
+        self.n_timesteps = DataLoader.n_timesteps
         self.scale = DataLoader.scale
-        self.n_batch = n_batch
         self.airport_index = airport_index
-        self.series_index = series_index
+
+        self.n_batch = DataLoader.n_batch
+        self.n_training = DataLoader.n_training
+        self.n_pred = DataLoader.n_pred
+        self.start_day = DataLoader.start_day
 
     def inverse_transform(self, input):
-        self.pred_transformed = []
-        for i in range(self.n_timesteps):
-            traffic = self.scale.inverse_transform(input[i,:].reshape(77,3).T).T[self.airport_index,self.series_index]
-            self.pred_transformed.append(traffic)
+        self.predicted_transformed = self.scale.inverse_transform(input[-1,:].reshape(77,self.n_pred).T).T[self.airport_index,:]
 
-    def plot(self, actual):
-        padded = np.pad(np.array(self.pred_transformed), (self.n_batch, 0))
-        self.padded = padded
-        value = np.append(padded, actual)
-        self.value = value
-        day = np.tile(np.arange(1, self.n_timesteps+self.n_batch+1), (2))
-        type = np.concatenate((np.array(['Predicted']*(self.n_timesteps+self.n_batch)), np.array(['Actual']*(self.n_timesteps+self.n_batch))))
-        atl_plot = pd.DataFrame(np.vstack((value, day, type)).T)
-        atl_plot.columns = ['Value', 'Day', 'Type']
-        atl_plot[['Value', 'Day']] = atl_plot[['Value', 'Day']].astype(float)
+        self.predicted_transformed = []
+        for i in range(self.n_batch):
+            traffic = self.scale.inverse_transform(input[i,:].reshape(77,self.n_pred).T).T[self.airport_index, self.n_pred-1]
+            self.predicted_transformed.append(traffic)
+
+    def plot(self):
+        x_train_start = self.start_day
+        y_train_start = self.start_day
+        x_train_end = x_train_start + self.n_training
+        y_train_end = y_train_start + self.n_training
+
+        x_test_start = self.start_day+self.n_training-1
+        x_test_end = x_test_start+self.n_pred+1
+        y_test_start = self.start_day+self.n_training-1
+        y_test_end = x_test_start+self.n_pred+1
+
+        x_forecast_start = 2*self.n_pred+self.start_day+self.n_timesteps
+        x_forecast_end = x_forecast_start + self.n_batch
+        
         fig, ax = plt.subplots()
-        sns.lineplot(data=atl_plot, x='Day', y='Value', hue='Type', ax=ax)
-        ax.set(title='ATL Throughput - Training Results', xlabel='Days Since 1/1/20', ylabel='# of Flights')
+        sns.lineplot(x=np.arange(x_train_start, x_train_end), 
+                     y=self.actual[self.airport_index, np.arange(y_train_start, y_train_end)], 
+                     label='Training',
+                     ax=ax)
+        sns.lineplot(x=np.arange(x_test_start, x_test_end), 
+                     y=self.actual[self.airport_index, np.arange(y_test_start, y_test_end)], 
+                     label='Testing - Actual',
+                     ax=ax)
+        sns.lineplot(x=np.arange(x_forecast_start, x_forecast_end), 
+                     y=self.predicted_transformed, 
+                     color='red', 
+                     marker='o', 
+                     ms=5, 
+                     label='Testing - Predicted',
+                     ax=ax)
+        ax.set(title='LSTM Forecast for Throughout at ATL',
+               ylim=(0,1600), 
+               xlim=(x_train_start, x_forecast_end+7))
+        plt.axvline(x=x_train_end-1, color='black', linestyle='--', linewidth=0.75)
+        plt.legend()
